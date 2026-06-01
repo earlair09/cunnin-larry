0d54823baa03303f51d7849abb53f4e0731a38c6
earlair09 <bearlair@shentel.net>
2026-06-01 11:05:26 -0700

    w4fv

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+app.use(express.json());
+
+const transporter =
+nodemailer.createTransport({
+
+host:
+process.env.SMTP_HOST,
+
+port:
+Number(
+process.env.SMTP_PORT
+),
+
+secure:
+false,
+
+auth: {
+
+user:
+process.env.SMTP_USER,
+
+pass:
+process.env.SMTP_PASS
+
+}
+
+});
+
+app.post(
+"/send",
+async (req,res)=>{
+
+try{
+
+const {
+
+to,
+subject,
+html,
+text
+
+}=req.body;
+
+await transporter.sendMail({
+
+from:
+process.env.SMTP_USER,
+
+to,
+
+subject,
+
+html,
+
+text
+
+});
+
+res.send({
+ok:true
+});
+
+}
+
+catch(e){
+
+res.status(500)
+.send(
+e.message
+);
+
+}
+
+});
+
+app.listen(
+3000,
+()=>console.log(
+"SMTP running"
+)
+);
